a2be0368bb7c6ea9a15e551a9ceb6471d3cc8514
natedog2237 <92417931+natedog2237@users.noreply.github.com>
2022-08-26 01:32:59 -0500

    Update Driver.java

--- codingInterview/src/codingInterview/Driver.java
+++ codingInterview/src/codingInterview/Driver.java
@@ -3,7 +3,7 @@ package codingInterview;
 public class Driver {
 
 	public static void main(String[] args) {
-		Solution2 newSolution = new Solution2();
+		Solution1 newSolution = new Solution1();
 		System.out.println(newSolution.isValid(new int[]{11,1,8,16,14 },5));
 	}
 
